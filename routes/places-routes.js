ab70582fb498acfcce59fd1354fd73847fa0b225
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-22 12:59:01 +0530

    add routes to place

---
+++ routes/places-routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { check } = require("express-validator");
+const router = express.Router();
+const placeController = require("../controllers/places-controllers");
+
+router.get("/:pid", placeController.getPlaceById);
+router.get("/user/:uid", placeController.getPlaceByUserId);
+router.post(
+  "/",
+  [
+    check("title")
+      .not()
+      .isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address")
+      .not()
+      .isEmpty()
+  ],
+  placeController.createPlace
+);
+
+router.patch(
+  "/:pid",
+  [
+    check("title")
+      .not()
+      .isEmpty(),
+    check("description").isLength({ min: 5 })
+  ],
+  placeController.updatePlace
+);
+router.delete("/:pid", placeController.deletePlace);
+
+module.exports = router;
